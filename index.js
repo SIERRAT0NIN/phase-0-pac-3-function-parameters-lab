97a9466f6dce1a8cf8686396a7b88dd17ee273e5
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-07-19 22:11:35 -0700

    parameters lab

---
+++ index.js
@@ -0,0 +1,17 @@
+const name = "Alberto";
+const language = "JavaScript";
+
+function introduction(name = "Aki") {
+  return `Hi, my name is ${name}.`;
+}
+
+function introductionWithLanguage(name, language) {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(
+  name = "Gracie",
+  language = "JavaScript"
+) {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
